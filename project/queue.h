2f0179c63f21aa86237811ac242dc2be83e0186d
Serra Demir <serra.e.demir@outlook.com>
2026-08-11 12:18:56 -0400

    wrote queue.h

--- project/queue.h
+++ project/queue.h
@@ -1,1 +1,20 @@
 
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "maze.h"
+
+#define MAX_QUEUE_SIZE 1000
+
+typedef struct {
+    Position items[MAX_QUEUE_SIZE];
+    int front;
+    int rear;
+} Queue;
+
+void queue_init(Queue *queue);
+int queue_enqueue(Queue *queue, Position value);
+int queue_dequeue(Queue *queue, Position *value);
+int queue_is_empty(const Queue *queue);
+
+#endif
