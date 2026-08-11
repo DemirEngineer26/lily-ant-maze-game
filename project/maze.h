1552aed1304b284b568c03df867da6dea287cdae
Serra Demir <serra.e.demir@outlook.com>
2026-08-11 12:18:22 -0400

    wrote maze.h

--- project/maze.h
+++ project/maze.h
@@ -1,1 +1,27 @@
+#ifndef MAZE_H
+#define MAZE_H
 
+//Header guards prevent double inclusion errors
+
+typedef struct {
+    int row;
+    int col;
+} Position;
+
+//Maze struture, featuring a 50x50 grid 
+typedef struct {
+    char grid[50][50];
+    int rows; 
+    int cols;
+    Position lily_pos;
+    int score;
+} Maze;
+
+//function prototypes
+int maze_load(Maze, *maze, const char *filename);
+void maze_print(const Maze *maze);
+int maze_can_move(const Maze *maze, int row, int col);
+int maze_move_lily(Maze *maze, char direction);
+int maze_college_point(Maze *maze);
+
+#endif //MAZE_H
