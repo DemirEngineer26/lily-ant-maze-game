fc29553f0242b789d2dcf4d0ad9769d25646e04c
Serra Demir <serra.e.demir@outlook.com>
2026-08-11 12:18:40 -0400

    wrote queue.c

--- project/queue.c
+++ project/queue.c
@@ -1,1 +1,33 @@
+#include "queue.h"
 
+void queue_init(Queue *queue) {
+    queue->front = 0;
+    queue->rear = 0;
+    queue->size = 0;
+}
+
+int queue_is_empty(const Queue *queue) {
+    return queue->size == 0;
+}
+
+int queue_is_full(const Queue *queue) {
+    return queue->size == MAX_QUEUE_SIZE;
+}
+int queue_enqueue(Queue *queue, Position value) {
+    if (queue_is_full(queue)) {
+        return 0;
+    }
+    queue->items[queue->rear] = value;
+    queue->rear = (queue->rear+1) % MAX_QUEUE_SIZE;
+    queue->size++;
+    return 1;
+}
+
+int queue_dequeue(Queue *queue, Position *value) {
+    if (queue_is_empty(queue)) return 0;
+
+    *value = queue->items[queue->front];
+    queue->front = (queue->front + 1) % MAX_QUEUE_SIZE;
+    queue->size--;
+    return 1;
+}
